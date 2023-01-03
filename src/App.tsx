5d974efa53f9cf1c7b40abfef73bf1f07a6c9dab
Lettuce222 <k.miyachi222@gmail.com>
2023-01-03 20:24:58 +0900

    GraphQLに型を導入、

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 import './App.css'
-import { gql, useQuery } from '@apollo/client';
+import { TypedDocumentNode, useQuery } from '@apollo/client';
 import { List, ListItem, ListItemText, TextField } from '@mui/material';
 import * as React from 'react';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import { gql } from '../src/__generated__/gql';
+import { Exact, GetItemsQuery, Item, Task, TaskObjective, TaskObjectiveItem } from './__generated__/graphql';
+import { Box } from '@mui/system';
 
-const GET_ITEMS = gql`
+const GET_ITEMS: TypedDocumentNode<GetItemsQuery, Exact<{
+  name: string;
+}>> = gql(`
   query getItems($name: String!) { 
     items(name: $name) {
       id
@@ -18,23 +23,23 @@ const GET_ITEMS = gql`
       }
     }
   }
-`;
+`);
 
 const Data = ({ name }: { name: string }) => {
   if (name === '') return <p>Please Enter Item's name.</p>;
 
   const { loading, error, data } = useQuery(GET_ITEMS, { variables: { name } });
 
-  if (loading) return <p>Loading...</p>;
+  if (loading || !data) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
 
   return (
     <List>
       {data.items.map((item) => (
-        <ListItem>
+        <ListItem key={item?.name}>
           <ListItemText
-            primary={item.name}
-            secondary={item.usedInTasks.map((task) => (task.name)).join(', ')}
+            primary={item?.name}
+            secondary={item?.usedInTasks?.map((task) => task?.name).join(", ")}
           />
         </ListItem>
       ))}
